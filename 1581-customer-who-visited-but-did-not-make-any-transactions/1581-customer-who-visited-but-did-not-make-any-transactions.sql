f6f6a6f86e55d894e77851217c2e8f6023854ea9
patrycjamilo1 <162639316+patrycjamilo1@users.noreply.github.com>
2025-02-16 23:09:25 +0100

    Delete 1581-customer-who-visited-but-did-not-make-any-transactions directory

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++
@@ -1,8 +0,0 @@
-SELECT v.customer_id, COUNT(v.customer_id) AS "count_no_trans"
-FROM Visits v
-LEFT JOIN transactions t
-ON v.visit_id = t.visit_id
-WHERE t.visit_id IS NULL
-GROUP BY v.customer_id
-ORDER BY COUNT(customer_id)
-
