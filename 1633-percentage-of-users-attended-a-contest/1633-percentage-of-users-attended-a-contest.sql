e21257d6adbb7a6faefbf9a5a71186a3121164a3
patrycjamilo1 <162639316+patrycjamilo1@users.noreply.github.com>
2025-02-16 23:10:27 +0100

    Delete 1633-percentage-of-users-attended-a-contest directory

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++
@@ -1,6 +0,0 @@
-SELECT r.contest_id, ROUND((COUNT(r.user_id))/(SELECT COUNT(user_id) FROM Users) * 100, 2) AS "percentage"
-FROM Register r
-LEFT JOIN Users u
-ON u.user_id = r.user_id
-GROUP BY r.contest_id
-ORDER BY "percentage" DESC, contest_id
