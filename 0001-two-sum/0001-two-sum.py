6ddc8fedefedc2e40c772d1b38671a6531645105
patrycjamilo1 <162639316+patrycjamilo1@users.noreply.github.com>
2025-02-22 20:48:05 +0100

    Time: 1 ms (59.8%), Space: 19.2 MB (11.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        lista = []
+        słownik = {}
 
         for i in range(len(nums)):
-            for j in range(len(nums) - 1):
-                if ((i != j) and (nums[i] + nums[j]) == target):
-                    lista.append(i)
-                    lista.append(j)
-                    return lista
-        
-        print(lista)
-                
-            
-        
+            liczba = target - nums[i]
+            if liczba in słownik:
+                return [słownik[liczba], i] 
+            else:
+                słownik[nums[i]] = i 
+
+        return [] 
