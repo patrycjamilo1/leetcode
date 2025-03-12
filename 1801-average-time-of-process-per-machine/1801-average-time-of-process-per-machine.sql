b0ae02c273f9428995e4cce08ffc32fccdb145a5
patrycjamilo1 <162639316+patrycjamilo1@users.noreply.github.com>
2025-03-12 21:52:44 +0100

    Time: 1164 ms (9.12%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -4,4 +4,3 @@ JOIN Activity b
 ON (a.machine_id = b.machine_id AND a.process_id = b.process_id AND a.activity_type != b.activity_type)
 WHERE a.activity_type = 'start' AND b.activity_type = 'end'
 GROUP BY a.machine_id
-ORDER BY a.machine_id
